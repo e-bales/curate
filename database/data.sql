6d8303abac97804a7c9718011c212a38c4577a77
Eli Bales <elibales21@gmail.com>
2023-07-28 15:29:04 +0000

    prepared for deployment

--- database/data.sql
+++ database/data.sql
@@ -9,3 +9,15 @@
 --      ('Learn to code', false),
 --      ('Build projects', false),
 --      ('Get a job', false);
+
+insert into "users" ("username", "password")
+  values ('dbTest', 'abc123'),
+         ('person2', 'password123');
+
+insert into "favorites" ("userId", "artId")
+  values ('1', '436964'),
+         ('2', '436829');
+
+insert into "followers" ("userId", "followedUserId")
+  values ('1', '2'),
+         ('2', '1');
