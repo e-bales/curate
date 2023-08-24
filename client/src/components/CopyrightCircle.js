30a54c5bf5646a56c21bf1e399a14f4487fa6f59
Eli Bales <elibales21@gmail.com>
2023-08-24 22:31:55 +0000

    added alert functionality to icon

--- client/src/components/CopyrightCircle.js
+++ client/src/components/CopyrightCircle.js
@@ -2,10 +2,19 @@ import './CopyrightCircle.css';
 import { BsCCircle } from 'react-icons/bs';
 
 export default function CopyrightCircle() {
+  function alertCopyright() {
+    alert(
+      "Metropolitan Museum of Art is unable to share images of this piece with third-parties. Please click 'More Info' on the artwork's page to be redirected to their site."
+    );
+  }
+
   return (
     <div className="copyright-wrap">
       <div className="copyright">
-        <BsCCircle className="copyright-icon" />
+        <BsCCircle
+          onClick={() => alertCopyright()}
+          className="copyright-icon hover-pointer"
+        />
       </div>
     </div>
   );
