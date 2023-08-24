b9a134ca7645831efe2575bc0671aa8d810fe57e
Eli Bales <elibales21@gmail.com>
2023-08-24 22:20:20 +0000

    added copyright symbol to unavailable pieces

---
+++ client/src/components/CopyrightCircle.js
@@ -0,0 +1,12 @@
+import './CopyrightCircle.css';
+import { BsCCircle } from 'react-icons/bs';
+
+export default function CopyrightCircle() {
+  return (
+    <div className="copyright-wrap">
+      <div className="copyright">
+        <BsCCircle className="copyright-icon" />
+      </div>
+    </div>
+  );
+}
